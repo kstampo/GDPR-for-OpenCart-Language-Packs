4485ea2600c872b2336dcdb5bf735a424a093331
kstampo <kstampo@gmail.com>
2018-05-28 23:58:33 +0300

    Create isenselabs_gdpr.php
    ...Work on progress. Check back soon!

---
+++ greek/src_2.0.x-2.2/upload/admin/language/greek/module/isenselabs_gdpr.php
@@ -0,0 +1,175 @@
+<?php
+// Heading
+$_['heading_title']    			= 'Συμμόρφωση GDPR';
+
+// Text
+$_['text_extension']   			= 'Επεκτάσεις';
+$_['text_success']     			= 'Επιτυχία: Έχετε πεξεργαστεί τη Συμμόρφωση GDPR!';
+$_['text_edit']        			= 'Επεξεργασία Module';
+$_['text_module']      			= 'Modules';
+$_['text_complete']    			= 'Ολοκληρωμένο';
+$_['text_error']       			= 'Σφάλμα';
+
+// Entry
+$_['entry_status']     			= 'Κατάσταση';
+$_['text_gdpr_status']			= 'Κατάσταση επέκτασης';
+$_['text_gdpr_status_helper']   = 'Ενεργοποίηση ή απενεργοποίηση επέκτασης';
+
+// Errors
+$_['error_permission'] 			= 'Προειδοποίηση: Δεν έχετε άδεια να επεξεργαστείτε το module!';
+$_['error_unexpected'] 			= 'Συνέβει ένα αναπάντεχο σφάλμα. Προσπαθήστε ξανά ή επικοινωνείστε με τους προγραμματιστές της επέκτασης αναφέροντας τον κωδικό σφάλματος: ';
+
+// Licensing
+$_['text_your_license']         = 'Η άδειά σας';
+$_['text_please_enter_the_code']= 'Παρακαλώ εισάγετε τον αριθμό της άδειάς σας:';
+$_['text_activate_license']     = 'Ενεργοποίηση Άδειας';
+$_['text_not_having_a_license'] = 'Δεν έχετε κωδικό; Αποκτήστε από εδώ.';
+$_['text_license_holder']       = 'License Holder';
+$_['text_registered_domains']   = 'Registered domains';
+$_['text_expires_on']           = 'License Expires on';
+$_['text_valid_license']        = 'VALID LICENSE';
+$_['text_manage']               = 'manage';
+$_['text_get_support']          = 'Get Support';
+$_['text_community']            = 'Community';
+$_['text_ask_our_community']    = 'We have a big community. You are free to ask it about your issue on the forum.';
+$_['text_browse_forums']        = 'Browse forums';
+$_['text_tickets']              = 'Tickets';
+$_['text_open_a_ticket']        = 'Want to communicate one-to-one with our tech people? Then open a support ticket.';
+$_['text_open_ticket_for_real'] = 'Open a ticket';
+$_['text_pre_sale']             = 'Pre-sale';
+$_['text_pre_sale_text']        = 'Have a brilliant idea for your webstore? Our team of developers can make it real.';
+$_['text_bump_the_sales']       = 'Bump the sales';
+
+$_['tab_home']       			= 'Λίστα ελέγχου';
+$_['tab_controlpanel'] 			= 'Ρυθμίσεις';
+$_['tab_support']       		= 'Υποστήριξη';
+$_['tab_requests']				= 'Αιτήματα';
+$_['tab_acceptances']			= 'Αποδοχή Όρων Πώλησης';
+$_['tab_personaldata']			= 'Third-Party';
+$_['tab_datasecurity']			= 'Ασφάλεια δεδομένων';
+
+$_['text_loading_data']		 	= 'Φόρτωση, παρακαλώ περιμένετε...';
+$_['text_no_results']			= 'No results in the current view.';
+
+$_['column_request_id']			= 'Α/α αιτήματος';
+$_['column_type']				= 'Τύπος';
+$_['column_email']				= 'Email';
+$_['column_accept_language']	= 'Γλώσσα';
+$_['column_user_agent']			= 'Περιηγητής';
+$_['column_server_ip']			= 'IP του Server';
+$_['column_client_ip']			= 'IP Πελάτη';
+$_['column_date']				= 'Ημερομηνία';
+$_['column_acceptance_id']		= 'Α/Α Αποδοχής';
+$_['column_name']				= 'Όνομα';
+$_['column_content']			= 'Περιεχόμενο';
+$_['column_actions']			= 'Ενέργειες';
+
+$_['text_date_start']			= 'Από Ημερομηνία';
+$_['text_date_end']				= 'Έως Ημερομηνία';
+$_['text_filter']				= 'Φίλτρο';
+$_['text_download']				= 'Κατέβασμα';
+$_['text_view_current']			= 'Δείτε τους τρέχοντες όρους';
+
+$_['text_life_of_links']		= 'Λήξη αιτημάτων GDPR';
+$_['text_access_to_personal_data_link'] = 'Πρόσβαση σε Προσωπικά Δεδομένα';
+$_['text_access_to_personal_data_link_helper'] = 'Προθεσμία (σε ώρες) κάθε αιτήματος για την ανάκτηση προσωπικών δεδομένων. Εάν ο πελάτης δεν επιβεβαιώσει μέσα σε αυτό το χρονικό διάστημα, θα πρέπει να κάνει νέο αίτημα.';
+$_['text_access_default_hours'] = 'Προεπιλεγμένη τιμή: 5 ώρες';
+
+$_['text_remove_personal_data_link'] = 'Αφαίρεση Προσωπικών Δεδομένων';
+$_['text_remove_personal_data_link_helper'] = 'Προθεσμία (σε ώρες) για τη διαγραφή και μετατροπή σε ανώνυμα των προσωπικών δεδομένων του πελάτη. Εάν ο πελάτης δεν επιβεβαιώσει σε αυτό το χρονικό διάστημα θα πρέπει να κάνει νέο αίτημα.';
+$_['text_delete_default_hours'] = 'Προεπιλεγμένη τιμή: 2 ώρες';
+
+$_['text_access_to_personal_data'] = 'Πρόσβαση σε προσωπικά δεδομένα';
+$_['text_access_to_personal_data_helper'] = 'Πρέπει να αναφέρετε σε ποιες υπηρεσίες ή sites στέλνετε τα προσωπικά δεδομένα των πελατών (π.χ. όνομα, email, α/α πελάτη, IP κλπ.) στα πεδία παρακάτω.';
+$_['text_third_party_services'] = 'Υπηρεσίες τρίτων';
+$_['text_third_party_services_helper'] = 'Αναφέρετε εδώ τις υπηρεσίες / sites που χρησιμοποιείτε για να καταγράφετε, αναλυετε ή εμπλουτίζετε την εμπειρία περιήγησης των πελατών σας. Παράδειγμα: Facebook Ads, Facebook Pixel, Google Analytics, Google AdWords, Google Retargeting, MailChimp, κλπ.';
+$_['text_other_services'] = 'Άλλες Υπηρεσίες';
+$_['text_other_services_helper'] = 'Αναφέρετε εδώ τις υπηρεσίες που δεν είναι διαθέσιμες στο κοινό ή ο,τιδήποτε χρησιμοποιείτε αυστηρά για εσωτερική χρήση. Παράδειγμα: Συνεργασία με σύστημα ERP, PIWIK, Matomo, κλπ.';
+
+$_['text_general'] = 'Γενικές Ρυθμίσεις';
+$_['text_show_gdpr_menu_left'] = 'Δείξε έναν υπερσύνδεσμο για την επέκταση στο Μενού επιλογών του Διαχειριστή';
+$_['text_show_gdpr_menu_left_helper'] = 'Ενεργοποιήστε το εάν θέλετε να βλέπετε έναν υπερσύνδεσμο για την επέκταση GDPR στο μενού επιλογών του Διαχειριστή.';
+$_['text_enabled'] = 'Ενεργοποιημένο';
+$_['text_disabled'] = 'Απενεργοποιημένο';
+$_['text_show_gdpr_menu_left_default'] = 'Προεπιλεγμένη τιμή: Απενεργοποιημένο';
+
+$_['tab_cookieconsent'] = 'Αποδοχή Cookies';
+$_['text_cc_enable'] = 'Εμφανίστε τη μπάρα αποδοχής Cookies';
+$_['text_cc_enable_helper'] = 'Επιλέξτε εάν θέλετε να ενεργοποιήσετε ή να απενεργοποιήσετε την μπάρα αποδοχής cookies.';
+$_['text_cc_positioning'] = 'Θέση';
+$_['text_cc_positioning_helper'] = 'Επιλέξτε τη θέση της μπάρας αποδοχής.';
+$_['text_cc_banner_bottom'] = 'Κάτω από το Banner';
+$_['text_cc_banner_top'] = 'Πάνω από το Banner';
+$_['text_cc_banner_top_pushdown'] = 'Πάνω από το Banner (pushdown)';
+$_['text_cc_banner_float_left'] = 'Αριστερά';
+$_['text_cc_banner_float_right'] = 'Δεξιά';
+$_['text_cc_link_to_pp'] = 'Εμφανίστε υπερσύνδεσμο για την πολιτική προσωπικών δεδομένων στη μπάρα';
+$_['text_cc_link_to_pp_helper'] = 'If enabled, will display a link to the privacy policy in the bar.';
+$_['text_cc_texts'] = 'Custom Texts';
+$_['text_cc_texts_helper'] = 'Define the texts which you wish to display in the cookie consent bar.';
+$_['text_cc_main_message'] = 'Main Message';
+$_['text_cc_main_message_helper'] = 'Enter the main message of the bar here. An example is already shown.';
+$_['text_cc_pp_message'] = 'Privacy Policy Link Text';
+$_['text_cc_pp_message_helper'] = 'If you choose to display a link to the policy in the bar, specify the text of the link here.';
+$_['text_cc_accept_text'] = 'Accept Button Text';
+$_['text_cc_accept_text_helper'] = 'Define the text on the accept button.';
+$_['text_cc_dismiss_text'] = 'Dismiss Button Text';
+$_['text_cc_dismiss_text_helper'] = 'Define the text on the dismiss button.';
+$_['text_cc_track_pp_clicks'] = 'Track Clicks as Privacy Policy Acceptances';
+$_['text_cc_track_pp_clicks_helper'] = 'If enabled, the clicks on the "I agree" button will be considered as privacy policy acceptances and they will be logged in the module.';
+$_['text_cc_bar_bg'] = 'Banner Background';
+$_['text_cc_bar_text'] = 'Banner Text';
+$_['text_cc_button_bg'] = 'Button Background';
+$_['text_cc_button_text'] = 'Button Text';
+$_['text_cc_palette'] = 'Color Palette';
+$_['text_cc_palette_styling'] = 'Choose the styling of your cookie consent bar.';
+
+// Dashboard
+$_['text_steps_gdpr_compliant'] = 'Λίστα ελέγχου GDPR';
+$_['text_install_the_extension'] = 'Εγκατάσταση επέκτασης';
+$_['text_install_the_extension_helper'] = 'Σιγουρευτείτε ότι η επέκταση εγκαταστάθηκε και ότι όλα τα αρχεία μεταφορτώθηκαν στο ηλεκτρονικό σας κατάστημα.';
+$_['text_enable_the_extension'] = 'Ενεργοποίηση επέκτασης';
+$_['text_enable_the_extension_helper'] = 'Πηγαίνετε στην καρτέλα διαχείρισης και ενεργοποιήστε το πρόσθετο.';
+$_['text_track_policy_acceptances'] = 'Εκκίνηση καταγραφής Αποδοχής Όρων Πώλησης';
+$_['text_track_policy_acceptances_helper'] = 'This is part of the GDPR law, which requires you to know exactly when and on which policy your customers agreed to.';
+$_['text_respont_gdpr_requests'] = 'Start Taking/Responding to GDPR requests';
+$_['text_respont_gdpr_requests_helper'] = 'This is automatically handled by the extension. By the GDPR law, you are required to respond to all GDPR requests by 72 hours. Thanks to our extension, they will be taken into account instantly.';
+$_['text_update_pp'] = 'Update your Privacy Policy';
+$_['text_update_pp_helper'] = 'This is something that you have to do manually. Make sure to update your privacy policy and add that your store is now compliant with the GDPR law.<br /><br />The GDPR says that the information you provide to people about how you process their personal data must be: <ul><li>concise, transparent, intelligible and easily accessible;</li><li>written in clear and plain language, particularly if addressed to a child;</li><li>free of charge.</li></ul>';
+$_['text_enable_cookie_consent_bar'] = 'Enable the Cookie Consent Bar (optional)';
+$_['text_enable_cookie_consent_bar_helper'] = 'If you do not have a cookie consent bar, you can try our own. It is highly customizable and when someone clicks on the "I Agree" button, we are adding a record in our database that this particular customer agreed to your privacy policy.<br /><br />You can enable our consent bar from the "Cookie Consent" tab in the settings of the module.';
+
+$_['text_consent_bar_copyright'] = '* The Cookie Consent Bar was created using the source code of cookieconsent.insites.com, which is originally developed by SilkTide Ltd.';
+$_['text_show_gdpr_footer'] = 'Εμφανίστε σύνδεσμο για την επέκταση στο κάτω μέρος της σελίδας (Footer)';
+$_['text_show_gdpr_footer_helper'] = 'Enable this if you want to see a link to the GDPR module in the footer of your store. Keep in mind that if your theme is highly modified, the link might not show up, and you have to add it manually.';
+
+$_['text_data_security_breach'] = 'Data Security Breach';
+$_['text_notify_customers'] = 'Notification of Data Security Breach';
+$_['text_data_security_breach_helper'] = 'Use this option to notify your customer when there is a data security breach.';
+$_['text_data_security_breach_helper_additional'] = 'You have to let your customers know exactly when the breach occured, what is the number of the affected accounts and what is(are) the action(s) that you have taken after the discovery.';
+
+$_['text_gdpr_checklist_helper'] = 'Please use the following personal GDPR checklist to mark each action you have accomplished. Click to check each point.';
+$_['text_gdpr_check_1'] = 'Have the GDPR compliance extension enabled';
+$_['text_gdpr_check_1_helper'] = 'This is the first easy step to GDPR compliance.';
+$_['text_gdpr_check_2'] = 'Make a new GDPR Link Show up in your Footer';
+$_['text_gdpr_check_2_helper'] = 'We have this feature. You can enable it from the <strong>Settings</strong> tab.';
+$_['text_gdpr_check_3'] = 'Test the GDPR Compliance Extension on the Store Front';
+$_['text_gdpr_check_3_helper'] = 'We add you a new section on the store front. Once the extension is enabled, your customers and visitors will have access to a new set of GDPR-related features. Please test how the GDPR extension front-end is visualized on your front-end - [link] - and if it looks good on your theme. You can also give a test to the user features.';
+$_['text_gdpr_check_4'] = 'Update your Privacy Policy with GDPR Information';
+$_['text_gdpr_check_4_helper'] = 'We will track your Privacy Policy acceptance. You just need to make sure your webstore Privacy Policy has a GDPR section. You can find your <strong>Privacy Policy</strong> page here - [link]. If you do not have such page, please create one from here - [link2]. You can find good examples of what you need to include in your privacy policy [linkstart]in this blog post[linkend].';
+$_['text_gdpr_check_5'] = 'Update your Terms and Conditions page with GDPR information';
+$_['text_gdpr_check_5_helper'] = 'We will track your Terms and Conditions page acceptance. You just need to make sure your webstore Terms and Conditions page has a GDPR section. You can find your <strong>Terms and Conditions</strong> page here - [link]. If you do not have such page, please create one from here - [link2]. You can find good examples of what you need to include in your terms &amp; conditions [linkstart]in this blog post[linkend].';
+$_['text_gdpr_check_6'] = 'Allow your Customers to Have Access to Personal Data';
+$_['text_gdpr_check_6_helper'] = 'We have this feature. Your customers can request and download personal data. You can test drive it here by testing with a customer profile - [link]';
+$_['text_gdpr_check_7'] = 'Allow your Customers to be Forgotten';
+$_['text_gdpr_check_7_helper'] = 'We have this feature. You can test drive it on the store front here - [link]';
+$_['text_gdpr_check_8'] = 'Allow your Customers to be able to Edit their Data';
+$_['text_gdpr_check_8_helper'] = 'We have this feature. You can test drive it on the store front here - [link]';
+$_['text_gdpr_check_9'] = 'Start using Cookie Consent if you still do not use it (Optional)';
+$_['text_gdpr_check_9_helper'] = 'We have this feature. You can enable it from the tab <strong>Cookie Consent</strong> and configure it according to your desired style.';
+$_['text_gdpr_check_10'] = 'Watch and Administer GDPR Requests and Acceptances';
+$_['text_gdpr_check_10_helper'] = 'We have this feature. You can do this in tabs <strong>Requests</strong> and <strong>Policy Acceptance</strong>.';
+$_['text_link'] = '[LINK]';
+$_['text_link_test'] = 'CLICK TO TEST';
+$_['text_link_view'] = 'CLICK TO VIEW';
+$_['text_link_add'] = 'CLICK TO ADD';
